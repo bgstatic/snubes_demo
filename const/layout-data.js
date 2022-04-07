e0d4b840737e150922a2dd3a18c371580c8780f5
bilgehangecici <bilgehangecici@gmail.com>
2022-04-07 11:50:10 +0300

    Add layout-data.js

---
+++ const/layout-data.js
@@ -0,0 +1,9 @@
+export const navigation = [
+    { label: 'About Us', isDropdown: false, url: '/about-us' },
+    { label: 'How it works', isDropdown: false, url: '/how-it-works' },
+    { label: 'Become a Partner', isDropdown: false, url: '/become-a-partner' },
+    { label: 'EUR', isDropdown: true, },
+    { label: 'EN', isDropdown: true, },
+    { label: 'Sign In', isDropdown: false, url: '/sign-in' },
+    { label: 'Get Access', isDropdown: false, url: '/get-access' },
+]
