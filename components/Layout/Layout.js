deabdb410f1730787be0347a78e9a84e362d87e6
bilgehangecici <bilgehangecici@gmail.com>
2022-04-07 22:29:48 +0300

    Update Layout Component

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
     return (
         <>
             <Header />
-            <main id="layout">
+            <main id="layout" className='overflow-hidden'>
                 <>
                     {children}
                 </>
