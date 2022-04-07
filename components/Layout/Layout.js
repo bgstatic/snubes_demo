77d45d651aca93ab8e7655f097ec7208c8b68b76
bilgehangecici <bilgehangecici@gmail.com>
2022-04-07 12:31:37 +0300

    Update Layout.js

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NextSeo } from "next-seo";
 import Header from '../Header'
 import Footer from '../Footer'
 
@@ -7,26 +6,6 @@ export default function Layout({ children }) {
 
     return (
         <>
-            <NextSeo
-                title="Snubes"
-                titleTemplate="Snubes"
-                defaultTitle="Snubes"
-                description="Successful Call Center Outsourcing Made Easy"
-                canonical="https://snubes.com/"
-                openGraph={{
-                    url: "https://snubes.com/",
-                    title: "Snubes",
-                    description: "Successful Call Center Outsourcing Made Easy",
-                    images: [
-                        {
-                            url: "images/components/Navbar/snubes-logo.svg",
-                            width: 800,
-                            height: 420,
-                            alt: "Snubes",
-                        },
-                    ],
-                }}
-            />
             <Header />
             <main id="layout">
                 <>
