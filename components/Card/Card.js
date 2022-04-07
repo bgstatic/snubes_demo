98c4a1b1399057731f6905c5544548c652a7acb4
bilgehangecici <bilgehangecici@gmail.com>
2022-04-07 11:44:16 +0300

    Add Card Component

---
+++ components/Card/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import * as styles from './card.module.scss'
+
+const Card = ({ title, description, logoSource }) => {
+    return (
+        <Container className={styles.container}>
+            <div className={styles.cardImageContainer}>
+                <img src={logoSource} alt={title} className={styles.logo} />
+            </div>
+            <div className={styles.cardTextContainer}>
+                <h1 className={styles.cardTitle}>{title}</h1>
+                <p className={styles.cardDescription}>{description}</p>
+            </div>
+        </Container >
+    )
+}
+
+export default Card
