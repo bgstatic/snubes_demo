4f0a9bc823fa22b21d3b283a4b91865e9748013f
bilgehangecici <bilgehangecici@gmail.com>
2022-04-07 11:44:33 +0300

    Add Footer Component

---
+++ components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Footer = () => {
+    return (
+        <Container>Footer</Container>
+    )
+}
+
+export default Footer
