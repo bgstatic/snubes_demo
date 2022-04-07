50b8776bf7b1b41a0ae1b47df8b71e0837a32c4a
bilgehangecici <bilgehangecici@gmail.com>
2022-04-07 22:58:48 +0300

    Add layout component

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
+import * as styles from './layout.module.scss'
 
 export default function Layout({ children }) {
 
     return (
         <>
             <Header />
-            <main id="layout" className='overflow-hidden'>
+            <main id="layout" className={`${styles.mainLayout}`}>
                 <>
                     {children}
                 </>
