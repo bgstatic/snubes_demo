df2519f325bb438aafca21c4646407c1b89f85c6
bilgehangecici <bilgehangecici@gmail.com>
2022-04-08 19:31:12 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import dynamic from "next/dynamic";
 import Features from '../sections/Features/'
 import WhySnubes from '../sections/WhySnubes/'
+import Testimonials from '../sections/Testimonials';
+import * as styles from '../styles/home.module.scss'
 
 const Layout = dynamic(() => import("../components/Layout"), { ssr: false });
 
@@ -14,8 +16,12 @@ export default function Home() {
         <meta name="description" content="Snubes Demo" />
         <link rel="icon" href="/snubes.ico" />
       </Head>
+      <div className={styles.line}></div>
       <Features />
+      <div className={styles.line}></div>
       <WhySnubes />
+      <div className={styles.line}></div>
+      <Testimonials />
     </Layout>
   )
 }
