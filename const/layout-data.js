131e3a22a6e14338292e44443a86d30a19ae91d4
bilgehangecici <bilgehangecici@gmail.com>
2022-04-12 10:06:17 +0300

    Update layout-data.js

--- const/layout-data.js
+++ const/layout-data.js
@@ -8,6 +8,37 @@ export const navigation = [
     { label: 'Get Access', isDropdown: false, url: '/get-access' },
 ]
 
+export const heroInformation = [
+    { title: '500+', description: 'Tenders' },
+    { title: '200+', description: 'Callcenter' },
+    { title: '375.000', description: 'Agents Available' },
+    { title: '85%', description: 'Fast sourcing' },
+]
+
+export const featuresInformation = [
+    {
+        title: 'Sign up for free',
+        description: 'Signing up and setting up your project takes less than 5 minutes',
+        logoSource: '/images/sections/Features/signup.svg',
+
+    },
+    {
+        title: 'Source smarter',
+        description: 'Our data-based AI and our experts will help you find exactly what you are looking for',
+        logoSource: '/images/sections/Features/source-logo.svg',
+    },
+    {
+        title: 'Receive offers',
+        description: 'With our insights you always receive high-quality proposals at the best price',
+        logoSource: '/images/sections/Features/offers.svg',
+    },
+]
+
+export const whySnubesInformation = [
+    { subtitle: 'Trusted Know-How', description: 'We have more than 20 years of experience and continuously check call centers for their quality and capabilities. Customers like Allianz trust Snubes industry knowledge.' },
+    { subtitle: 'Time and cost savings', description: 'With Snubes you can easily compare prices, quality and availability, and find more potential suppliers. Reach your goal faster and save resources.' },
+    { subtitle: 'Satisfaction guaranteed', description: 'We offer a validated data-based approach to procurement, rather than awarding contracts only based on the lowest price, an existing relationship or "gut feeling".' },
+]
 
 export const companies = [
     { label: 'Allianz', path: '/images/sections/Testimonials/allianz.jpg' },
@@ -16,4 +47,42 @@ export const companies = [
     { label: 'Laserhub', path: '/images/sections/Testimonials/laserhub.jpg' },
     { label: 'talixo', path: '/images/sections/Testimonials/talixo-gray.jpg' },
 ]
+
+export const frequentlyAskedQuestionsInformation = [
+    {
+        question: 'Are the call centers verified?',
+        answer: 'All call centers in our database are checked by quality assurance experts. We also take a look at their data and track the key performance indicators. In addition, clients rate call centers and share their customer experience.',
+    },
+    {
+        question: 'Which inbound services are offered at Snubes?',
+        answer: 'Our listed call centers offer a broad spectrum of customer services, such as technical support, first-level support, helpdesk, overflow/after-hours support, etc.',
+    },
+    {
+        question: 'Are there any costs?',
+        answer: 'The services of Snubes are free of charge and our call center experts advise you independently and personally. Together we strive to find the optimal solution.',
+    },
+]
+
+
+export const footerNavigation = {
+
+    firstColumn: [
+        { label: 'About Us', url: '/about-us' },
+        { label: 'Become a Partner', url: '/become-a-partner' },
+        { label: 'FAQ', url: '/faq' },
+    ],
+
+    secondColumn: [
+        { label: 'Imprint', url: '/imprint' },
+        { label: 'Terms & Conditions', url: '/terms-and-conditions' },
+        { label: 'Privacy Policy', url: '/privacy-policy' },
+    ],
+
+    thirdColumn: [
+        { label: 'support@snubes.com', url: 'mailto:support@snubes.com' },
+        { label: '+49 (0) 305 5645327', url: 'tel:+4903055645327' },
+    ],
+
+}
+
 
