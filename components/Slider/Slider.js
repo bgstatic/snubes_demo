2c0f31b08bd5f2c3f1957b87f1fba8c14741c028
bilgehangecici <bilgehangecici@gmail.com>
2022-04-12 13:28:08 +0300

    Update Slider

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -1,14 +1,12 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Container } from 'react-bootstrap';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination } from "swiper";
-// import './slider.css;
 import * as styles from './slider.module.scss';
 
 function Slider() {
 
     return (
-        // <Container className={styles.container}>
         <Swiper navigation={true} pagination={true} modules={[Navigation, Pagination]} className={styles.swiper}>
             <SwiperSlide className={styles.slideTest}>
                 <Container className={styles.sliderContainer}>
@@ -17,11 +15,21 @@ function Slider() {
                     <p className={styles.sliderCaption}><strong>Jan Brenneke,</strong> VP Operations</p>
                 </Container>
             </SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
+            <SwiperSlide>
+                <Container className={styles.sliderContainer}>
+                    <img className={styles.sliderLogo} src={"/images/components/Slider/talixo-black.jpg"} alt="talixo" />
+                    <p className={styles.sliderDescription}>“Finding a suitable outsourcing service provider through Snubes was very easy and our personal consultant helped us every step of the way. After a short time we had good offers on the table. An on-site visit confirmed the positive impression and we have now found a matching partner. Thank you for your great support, which has helped us a lot in the selection process.”</p>
+                    <p className={styles.sliderCaption}><strong>Jan Brenneke,</strong> VP Operations</p>
+                </Container>
+            </SwiperSlide>
+            <SwiperSlide>
+                <Container className={styles.sliderContainer}>
+                    <img className={styles.sliderLogo} src={"/images/components/Slider/talixo-black.jpg"} alt="talixo" />
+                    <p className={styles.sliderDescription}>“Finding a suitable outsourcing service provider through Snubes was very easy and our personal consultant helped us every step of the way. After a short time we had good offers on the table. An on-site visit confirmed the positive impression and we have now found a matching partner. Thank you for your great support, which has helped us a lot in the selection process.”</p>
+                    <p className={styles.sliderCaption}><strong>Jan Brenneke,</strong> VP Operations</p>
+                </Container>
+            </SwiperSlide>
         </Swiper>
-        // </Container>
-
     );
 }
 
