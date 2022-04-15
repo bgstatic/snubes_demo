20ba994a6be0c206ba00af706b8beec4bc0cac24
bilgehangecici <bilgehangecici@gmail.com>
2022-04-15 09:47:39 +0300

    Update config

--- postcss.config.js
+++ postcss.config.js
@@ -1,108 +1,28 @@
-const glob = require('glob-all');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const getCSSModuleLocalIdent = require('react-dev-utils/getCSSModuleLocalIdent');
-
-const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
-
-module.exports = function (webpackEnv) {
-
-    const isEnvDevelopment = webpackEnv === 'development';
-    const isEnvProduction = webpackEnv === 'production';
-    const shouldUseRelativeAssetPaths = publicPath === './';
-
-    const getStyleLoaders = (cssOptions, preProcessor) => {
-        const loaders = [
-            isEnvDevelopment && require.resolve('style-loader'),
-            isEnvProduction && {
-                loader: MiniCssExtractPlugin.loader,
-                options: shouldUseRelativeAssetPaths ? { publicPath: '../../' } : {}
-            },
+module.exports = {
+    "plugins": [
+        "postcss-flexbugs-fixes",
+        [
+            "postcss-preset-env",
             {
-                loader: require.resolve('css-loader'),
-                options: cssOptions
-            },
-            {
-                loader: require.resolve('postcss-loader'),
-                options: {
-                    ident: 'postcss',
-                    syntax: 'postcss-scss',
-                    plugins: () => [
-                        require('postcss-flexbugs-fixes'),
-                        require('postcss-preset-env')({
-                            autoprefixer: {
-                                flexbox: 'no-2009'
-                            },
-                            stage: 3
-                        }),
-                        require('@fullhuman/postcss-purgecss')({
-                            content: [paths.appHtml, ...glob.sync(path.join(paths.appSrc, '/**/*.{js,jsx}'), { nodir: true })],
-                            extractors: [
-                                {
-                                    extractor: class {
-                                        static extract(content) {
-                                            return content.match(/[\w-/:]+(?<!:)/g) || [];
-                                        }
-                                    },
-                                    extensions: ['html', 'js', 'jsx']
-                                }
-                            ]
-                        }),
-                        require('postcss-normalize')
-                    ].filter(Boolean),
-                    sourceMap: isEnvProduction && shouldUseSourceMap
+                "autoprefixer": {
+                    "flexbox": "no-2009"
+                },
+                "stage": 3,
+                "features": {
+                    "custom-properties": false
                 }
             }
-        ].filter(Boolean);
-        if (preProcessor) {
-            loaders.push({
-                loader: require.resolve(preProcessor),
-                options: {
-                    sourceMap: isEnvProduction && shouldUseSourceMap
-                }
-            });
-        }
-        return loaders;
-    };
-
-    return {
-
-        /* {...} */
-
-        module: {
-            rules: [
-
-                /* {...} */
-
-                {
-                    oneOf: [
-
-                        /* {...} */
-
-                        {
-                            test: /\.module\.(scss|sass)$/,
-                            use: getStyleLoaders(
-                                {
-                                    importLoaders: 2,
-                                    sourceMap: isEnvProduction && shouldUseSourceMap,
-                                    modules: true,
-                                    getLocalIdent: getCSSModuleLocalIdent
-                                },
-                                'sass-loader'
-                            )
-                        }
-
-                        /* {...} */
-
-                    ]
-                }
-
-                /* {...} */
-
-            ]
-        },
-
-        /* {...} */
-
-    };
-
-};
+        ],
+        [
+            '@fullhuman/postcss-purgecss',
+            {
+                content: [
+                    './pages/**/*.{js,jsx,ts,tsx}',
+                    './components/**/*.{js,jsx,ts,tsx}'
+                ],
+                defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+                safelist: ["html", "body"]
+            }
+        ],
+    ]
+}
